c06b5d7bf262e9f5208f52bc844c9384fe044fca
Marek Sagan <marek.sagan@gmail.com>
2019-01-13 19:33:53 +0100

    Made some cleans in code, add index.js in routes

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const express = require('express');
-const monsters = require('./routes/monsters');
 const bodyParser = require('body-parser');
-const habitats = require('./routes/habitats');
-const lives = require('./routes/lives');
+const routes = require('./routes');
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use('/monsters', monsters);
-app.use('/habitats', habitats);
-app.use('/lives', lives);
+app.use('/', routes);
 
 app.use((err, req, res, next) => {
     res.status(500).json(err);
