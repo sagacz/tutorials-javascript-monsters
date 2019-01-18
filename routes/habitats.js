4f352674549c10053a51f9ce6f68d9514f226d49
Marek Sagan <marek.sagan@gmail.com>
2019-01-18 17:06:20 +0100

    add put and delete rest method in habitats

--- routes/habitats.js
+++ routes/habitats.js
@@ -34,4 +34,34 @@ router.post('/', (request, response, next) => {
 });
 
 
+
+router.put('/:id', (request, response, next) => {
+    const { id } = request.params;
+    const keys = ['name', 'climate', 'temperature'];
+    const fields = [];
+
+    keys.forEach((key) => {
+        if(request.body[key]) fields.push(key);
+    });
+
+    fields.forEach((field, index) => {
+        pool.query(`UPDATE habitats SET ${field} = ($1) WHERE id = ($2)`, 
+            [request.body[field], id],
+            (err, res) => {
+                if(err) return next(err);
+
+                if(index == fields.length - 1) response.redirect('/habitats');
+            });
+    });
+});
+
+router.delete('/:id', (request, response, next) => {
+    const {id} = request.params;
+
+    pool.query(`DELETE FROM habitats WHERE id = ($1)`, [id], (err, res) => {
+        if(err) return next(err);
+        response.redirect('/habitats');
+    });
+
+});
 module.exports=router;
